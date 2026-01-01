752ea47e9cc9889e02fd982310babd2529dc8db2
greg-ogs <65621681+greg-ogs@users.noreply.github.com>
2026-01-01 17:22:09 -0600

    Update `gemma3_only_text.py` to support device detection and improve generation logic
    - Added device detection for CUDA support and updated model setup to use the detected device.

--- gemma3_only_text.py
+++ gemma3_only_text.py
@@ -5,6 +5,7 @@ Container for Gemma3-1b or slower
 from transformers import AutoTokenizer, Gemma3ForCausalLM, TextStreamer
 import torch
 import os
+import time
 
 model_id = "google/gemma-3-1b-it"
 
@@ -15,9 +16,13 @@ custom_cache_dir = "model_files/Gemma3"
 if hf_token is None:
     raise ValueError("HF_TOKEN environment variable not found. Please set it before running.")
 
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+print(f"Using device: {device}")
+
 model = Gemma3ForCausalLM.from_pretrained(
     model_id, token=hf_token, cache_dir=custom_cache_dir, dtype=torch.bfloat16
-).eval()
+).to(device).eval()
 
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 
@@ -31,7 +36,7 @@ messages = [
         },
         {
             "role": "user",
-            "content": [{"type": "text", "text": "Who was the newest f1 champion?"},]
+            "content": [{"type": "text", "text": "Who was the last f1 champion?"},]
         },
     ],
 ]
@@ -42,7 +47,6 @@ inputs = tokenizer.apply_chat_template(
     return_dict=True,
     return_tensors="pt",
 ).to(model.device)
-
 
 with torch.inference_mode():
     outputs = model.generate(**inputs, max_new_tokens=64, do_sample=True, streamer=streamer, temperature=0.3)
