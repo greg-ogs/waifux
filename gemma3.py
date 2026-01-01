799f1fc9a060f3e352a7e349c122a3f567743082
greg-ogs <65621681+greg-ogs@users.noreply.github.com>
2025-12-31 19:05:34 -0600

    Add initial Gemma-3 integration and dependencies
    - Added Gemma-3 script (`gemma3.py`) for image description tasks.
    - Included `tokens_requirements.txt` with required dependencies.
    - Updated `.gitignore` to stop tracking `.idea` workspace folder.

---
+++ gemma3.py
@@ -0,0 +1,57 @@
+"""
+
+"""
+# pip install accelerate
+
+from transformers import AutoProcessor, Gemma3ForConditionalGeneration
+from PIL import Image
+import requests
+import torch
+import os
+
+model_id = "google/gemma-3-12b-it"
+
+hf_token = os.getenv("HF_TOKEN")
+
+custom_cache_dir = "./model_cache/Gemma3"
+
+if hf_token is None:
+    raise ValueError("HF_TOKEN environment variable not found. Please set it before running.")
+
+model = Gemma3ForConditionalGeneration.from_pretrained(
+    model_id, device_map="auto", token=hf_token, cache_dir=custom_cache_dir
+).eval()
+
+processor = AutoProcessor.from_pretrained(model_id, token=hf_token, ache_dir=custom_cache_dir)
+
+messages = [
+    {
+        "role": "system",
+        "content": [{"type": "text", "text": "You are a helpful assistant."}]
+    },
+    {
+        "role": "user",
+        "content": [
+            {"type": "image", "image": "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/bee.jpg"},
+            {"type": "text", "text": "Describe this image in detail."}
+        ]
+    }
+]
+
+inputs = processor.apply_chat_template(
+    messages, add_generation_prompt=True, tokenize=True,
+    return_dict=True, return_tensors="pt"
+).to(model.device, dtype=torch.bfloat16)
+
+input_len = inputs["input_ids"].shape[-1]
+
+with torch.inference_mode():
+    generation = model.generate(**inputs, max_new_tokens=100, do_sample=False)
+    generation = generation[0][input_len:]
+
+decoded = processor.decode(generation, skip_special_tokens=True)
+print(decoded)
+
+# **Overall Impression:** The image is a close-up shot of a vibrant garden scene,
+# focusing on a cluster of pink cosmos flowers and a busy bumblebee.
+# It has a slightly soft, natural feel, likely captured in daylight.
