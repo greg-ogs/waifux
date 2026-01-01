bb6fec495e6c6001dafd616af85a883f59e05cae
greg-ogs <65621681+greg-ogs@users.noreply.github.com>
2026-01-01 01:13:55 -0600

    Add `gemma3_only_text.py` for text-only generation with Gemma-3
    - Introduced a new script focused on text-only tasks using Gemma-3-1b.
    - Integrated `TextStreamer` for token streaming and adjusted default generation parameters.

---
+++ gemma3_only_text.py
@@ -0,0 +1,50 @@
+"""
+Container for Gemma3-1b or slower
+"""
+
+from transformers import AutoTokenizer, Gemma3ForCausalLM, TextStreamer
+import torch
+import os
+
+model_id = "google/gemma-3-1b-it"
+
+hf_token = os.getenv("HF_TOKEN")
+
+custom_cache_dir = "model_files/Gemma3"
+
+if hf_token is None:
+    raise ValueError("HF_TOKEN environment variable not found. Please set it before running.")
+
+model = Gemma3ForCausalLM.from_pretrained(
+    model_id, token=hf_token, cache_dir=custom_cache_dir, dtype=torch.bfloat16
+).eval()
+
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+streamer = TextStreamer(tokenizer, skip_prompt=True)
+
+messages = [
+    [
+        {
+            "role": "system",
+            "content": [{"type": "text", "text": "You are a helpful assistant."},]
+        },
+        {
+            "role": "user",
+            "content": [{"type": "text", "text": "Who was the newest f1 champion?"},]
+        },
+    ],
+]
+inputs = tokenizer.apply_chat_template(
+    messages,
+    add_generation_prompt=True,
+    tokenize=True,
+    return_dict=True,
+    return_tensors="pt",
+).to(model.device)
+
+
+with torch.inference_mode():
+    outputs = model.generate(**inputs, max_new_tokens=64, do_sample=True, streamer=streamer, temperature=0.3)
+
+# outputs = tokenizer.batch_decode(outputs)
